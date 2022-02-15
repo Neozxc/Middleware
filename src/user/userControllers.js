e41ddecec20f6bb7be33fd1534555d06f8576c9f
Lapinas <mrlapinas99@gmail.com>
2022-02-15 17:25:37 +0000

    Changes

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -4,6 +4,7 @@ exports.addUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
         res.status(200).send({ user: newUser });
+        console.log("Successfully worked.");
     } catch (error) {
         console.log(error);
         res.status(500).send({ error: error.message });
