0f76efe2618dda994f2a228bdba35d499b8ba0bd
Lapinas <mrlapinas99@gmail.com>
2022-02-15 17:16:04 +0000

    Needed to add function

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -1,11 +1,11 @@
-const res = require("express/lib/response");
 const User = require("./userModel");
 
-exports.addUser = async () => {
+exports.addUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
         res.status(200).send({ user: newUser });
     } catch (error) {
         console.log(error);
+        res.status(500).send({ error: error.message });
     }
 };
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { addUser } = require("./userControllers");
-const { hasPass } = require("../middleware");
+const { hasPass } = require("../middleware/index");
 const userRouter = Router();
 
-userRouter.post("/user", hasPass, addUser);
+userRouter.post("/user", () => {
+    hasPass, addUser
+});
 
 module.exports = userRouter;
